01ab2435e55798f9c7c29bff3aa9883a7d963420
Baraa Mohamed <albaraa.m.m2005@gmail.com>
2025-11-25 10:10:18 +0200

    fix

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,7 +7,8 @@ namespace Shoppia.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        // test
+
+
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
