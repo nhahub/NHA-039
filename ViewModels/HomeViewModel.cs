a71dcadb7f9d25353acc6b795bb704eda3aba9ba
Alyaa-Ashraf <alyaagibreel@gmail.com>
2025-12-07 15:00:40 +0200

    connecting database with products &vendors

---
+++ ViewModels/HomeViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Shoppia.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Shoppia.Models.Product> FeaturedProducts { get; set; }
+        public IEnumerable<Shoppia.Models.Vendor> TopVendors { get; set; }
+    }
+}
